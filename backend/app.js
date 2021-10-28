9b0f85609040f3174855f1a62d8e96013518436e
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-10-28 21:38:49 +0200

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,36 @@
-const express = require('express');
+const express = require ('express');
+const mongoose = require('mongoose'); // Facilite les interactions avec la db
+const path = require('path');
+const bodyParser = require('body-parser');
+const helmet = require("helmet");
+const xss = require('xss-clean');
 
-const app = express();
+const clean = require('xss-clean/lib/xss').clean
+ 
+const cleaned = clean('<script></script>')
+// will return "&lt;script>&lt;/script>"
 
+
+const dotenv = require('dotenv').config()  // Charge la variable d'environnement
+
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+mongoose.connect('mongodb+srv://picante:picante@cluster0.n16ez.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(() => console.log('Conexion à MongoDB réussi !'))
+  .catch(() => console.log('connexion à MongoDB échouée !'))
+
+const app = express(); // Création d'une application express
+
+
+app.use(helmet());
+app.use(xss())
+
+ // Middleware appliqué à toutes les routes, permettant l'envoie de requête et d'accéder à l'API 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,26 +38,18 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
 
-module.exports = app;
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes); 
+app.use('/api/sauces', sauceRoutes); // Enregistrement du routeur pour toutes les demandes effectuées vers /api/sauces
+
+
+
+module.exports = app; // Donne l'accès depuis les autres fichiers, notamment le serveur Node
+
